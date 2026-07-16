2c537face289bb6d4912fd51c38342e78ca31b29
arafaths <arafathossensabbir6++++@gmail.com>
2026-07-16 09:27:45 +0600

    update

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-// Load environment variables heee
+// Load environment variables 
 dotenv.config();
 
 const app = express();
